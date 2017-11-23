be48a7b643ce2dc0947dfdcf1ae90999edc71ea6
Eddie Lao <eddielao@gmail.com>
2017-11-23 14:21:04 -0800

    Defined calculator test

--- Calculator/Calculator.Test/CalculatorTest.cs
+++ Calculator/Calculator.Test/CalculatorTest.cs
@@ -9,7 +9,16 @@ namespace Calculator.Test
         [TestMethod]
         public void Test_Divide()
         {
+            // Arrange
+            int expected = 3;
+            int numerator = 18;
+            int denominator = 6;
 
+            // Act
+            int actual = Calculator.Library.Calculator.Divide(numerator, denominator);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 Microsoft Unit Test Project
 
 # Introduction
+
+## AAA
+
+The AAA(Arrange, Act, Assert) pattern is a common way of writing unit tests.
+
+	*Arrange: Initializes objects and sets the value of the data that is passed to the method being tested
+
+	*Act: Invokes the method being tested
+
+	*Assert: Verifies that the method being tested behaves as expected
